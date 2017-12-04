b94dee94c003a4f76243592f850fc092c3a703c7
root <root@sbess111.hcqis.org>
2017-12-04 11:17:34 -0500

    adding dockerfile for restify service

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    API_ENDPOINT_URL: 'http://localhost:8080/rest'
+    API_ENDPOINT_URL: 'http://internal-ecsapplicationloadbalancer-1749096809.us-east-1.elb.amazonaws.com/rest'
 };
